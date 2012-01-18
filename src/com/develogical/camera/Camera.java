9f8d96d8ae5c58156e1515edfba2a143089421c6
Igor Kutsyy <igor.k22@gmail.com>
2012-01-18 15:29:23 +0000

    changes

--- src/com/develogical/camera/Camera.java
+++ src/com/develogical/camera/Camera.java
@@ -1,17 +1,77 @@
 package com.develogical.camera;
 
-public class Camera {
+
+
+public class Camera implements WriteListener{
+    
+    private Sensor sensor;
+    private MemoryCard memoryCard;
+
+    public enum cameraStatus {CAMERA_ON,CAMERA_OFF};
+    public enum MemoryWriteStatus {WRITE_NOT_COMPLETE,WRITE_COMPLETE}
+    private cameraStatus currentStatus;
+    private MemoryWriteStatus isWriteComplete;
+
+
+    public Camera(Sensor sensor,MemoryCard memoryCard){
+        
+        this.sensor = sensor;
+        this.memoryCard = memoryCard;
+        currentStatus = cameraStatus.CAMERA_OFF;
+        isWriteComplete = MemoryWriteStatus.WRITE_COMPLETE;
+
+
+        
+        
+    }
+    
+    public void writeComplete(){
+
+        isWriteComplete = MemoryWriteStatus.WRITE_COMPLETE;
+        
+        
+    }
 
     public void pressShutter() {
         // not implemented
+
+        if (currentStatus == cameraStatus.CAMERA_OFF) {
+
+        }
+
+        if(currentStatus == cameraStatus.CAMERA_ON){
+
+
+            byte[] data = sensor.readData();
+
+            if((data!=null) && (data.length>=1)){
+                memoryCard.write(data);
+                isWriteComplete = MemoryWriteStatus.WRITE_NOT_COMPLETE;
+            }
+
+        }
+
+
+
     }
 
     public void powerOn() {
-        // not implemented
+
+
+        sensor.powerUp();
+        currentStatus = cameraStatus.CAMERA_ON;
+
     }
 
     public void powerOff() {
-       // not implemented
+
+        if(isWriteComplete==MemoryWriteStatus.WRITE_COMPLETE){
+            sensor.powerDown();
+        }
+        currentStatus = cameraStatus.CAMERA_OFF;
+
+
     }
+
 }
 
--- test/com/develogical/camera/TestCamera.java
+++ test/com/develogical/camera/TestCamera.java
@@ -11,10 +11,139 @@ import java.lang.reflect.Member;
 @RunWith(value = JMock.class)
 public class TestCamera {
 
+    
+    Mockery context = new Mockery();
+    
+    Sensor sensor = context.mock(Sensor.class);
+    MemoryCard memoryCard = context.mock(MemoryCard.class);
+    WriteListener writeListener = context.mock(WriteListener.class);
+    Camera camera = new Camera(sensor,memoryCard);
+
+    
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-
+        
+        context.checking(new Expectations() {{
+            exactly(1).of(sensor).powerUp();
+        }}
+        );
+        
+        camera.powerOn();        
+        
+        
+        
          // write you test here
     }
+    
+    @Test
+    public void switchingTheCameraOffPowersDownTheSensor(){
+        
+        context.checking(new Expectations() {{
+            exactly(1).of(sensor).powerDown();
+            //exactly(1).of(writeListener).writeComplete();
+        }});
+
+
+        camera.powerOff();
+        
+        
+    }
+    
+    @Test
+    public void pressTheCameraShutterWhenPowerIsOff(){
+
+        //assumed power is off at the moment;
+        context.checking(new Expectations(){{
+            never(sensor);
+        }});
+
+        camera.pressShutter();
+    }
+
+    @Test
+    public void pressTheCameraShutterWhenCameraIsOn()
+    {
+        //pressing the shutter with the power on copies data from the sensor to the memory card
+
+
+        final byte[] data = {0,1};//some data
+        Camera camera = getCameraPoweredOn();
+
+
+
+        
+        context.checking(new Expectations(){
+            {
+                exactly(1).of(sensor).readData(); will(returnValue(data));
+                exactly(1).of(memoryCard).write(data);
+        }
+        });
+
+        camera.pressShutter();
+        
+    }
+
+    private Camera getCameraPoweredOn() {
+        Camera camera = new Camera(sensor, memoryCard);
+
+        context.checking(new Expectations() {
+            {
+                exactly(1).of(sensor).powerUp();
+            }
+        });
+
+        camera.powerOn();
+        return camera;
+    }
+
+
+    
+    @Test
+    public void switchingTheCameraOffWhenDataIsWrittenDoesNotPowerDownTheSensor(){
+        //if data is currently being writte, swithcing the camera off does not power down the sensor    
+
+        Camera camera = getCameraPoweredOn();
+
+        getCameraPressShutter(camera);
+
+        context.checking(new Expectations(){{
+            never(sensor).powerDown();
+        
+        }
+        });
+        
+        
+        camera.powerOff();
+
+    }
+
+    private void getCameraPressShutter(Camera camera) {
+
+
+        final byte[] data = {0,1};//some data
+
+        context.checking(new Expectations(){
+            {
+                exactly(1).of(sensor).readData(); will(returnValue(data));
+                exactly(1).of(memoryCard).write(data);
+            }
+        });
+
+        camera.pressShutter();
+    }
+
+
+    @Test
+    public void switchingTheCameraOffWhenWritingComplete(){
+
+        Camera camera = getCameraPoweredOn();
+
+        getCameraPressShutter(camera);
+
+
+
+
+    }
+
 
 }
